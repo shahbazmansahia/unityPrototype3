6715e079b375b6c192d1031b76da9e8b1a59173e
shahbazmansahia <shahbaz_mansahia@yahoo.com>
2021-07-07 05:22:16 -0700

    Developed the SpawnManger. Obstacles now spawn at a regular rate. Also fixed the player tranformation on collision and locked him/her in place via rigidbody properties.

--- SpawnManager.cs
+++ SpawnManager.cs
@@ -6,15 +6,25 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     public Vector3 spawnPos = new Vector3(25, 0, 0);
+
+    private float repeatRate = 2.0f;
+    private float startDelay = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        // Invokes the obstacle spawn function every <repeatRate> seconds after a delay of <startDelay>
+        InvokeRepeating("spawnObstacle", startDelay, repeatRate);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void spawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
